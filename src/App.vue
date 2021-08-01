8375c2e609aa4e802cd6a24c0b878a360694f313
Diego <ddglobaltravel1@gmail.com>
2021-08-01 02:38:54 -0500

    App del clima, barra de busqueda agregada

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-main">
+    <Search @search-input="setQuery" @keypress="searchQuery"></Search>
+    {{ query }}
+    <Weather></Weather>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Search from '@/components/Search'
+import Weather from '@/components/Weather'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Weather,
+    Search
+  },
+  data () {
+    return {
+      api_key: 'c1116db5f8a5420c8c8204718213107',
+      url_base: 'http://api.weatherapi.com/v1/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    setQuery (country) {
+      this.query = country
+    },
+    searchQuery (e) {
+      if (e.key === 'Enter') {
+        console.log('Estoy buscando')
+        alert('Buscaste ' + this.query)
+      }
+    }
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  background-image: url("./assets/bg-warm.jpg");
+  background-size: cover;
+  min-height: 100vh;
+  transition: 0.4s;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+}
+
+.container-main {
+  min-height: 100vh;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
 }
 </style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="weather-container">
+    <div class="location-box">
+      <div class="location">Caracas, Venezuela</div>
+      <div class="date">29 de Octubre</div>
+    </div>
+    <div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Weather'
+}
+</script>
+
+<style scoped>
+.weather-container {
+  display: flex;
+  flex-flow: column nowrap;
+  min-height: 20vw;
+  justify-content: center;
+}
+.location-box .location {
+  font-size: 8vmin;
+  font-weight: bold;
+  text-align: center;
+  text-transform:  uppercase;
+}
+.location-box .date {
+  font-size: 5vmin;
+  font-weight: 400;
+  font-style: italic;
+  text-align: center;
+  text-transform: uppercase;
+}
+
+</style>
